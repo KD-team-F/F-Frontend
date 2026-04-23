b1371f5a6e17b63bfb235f8881c5b6baee70a3ef
Hibi323223 <kd1367623@st.kobedenshi.ac.jp>
2026-04-24 08:10:48 +0900

    ヘッダーとフッターの修正

--- src/components/ui/Footer/Footer.tsx
+++ src/components/ui/Footer/Footer.tsx
@@ -3,11 +3,11 @@ export function Footer() {
     <footer className="bg-[#ABE1FA] px-4 py-8 mt-auto">
       <div className="mx-auto max-w-7xl flex flex-col items-center gap-6">
         
-       {/* // TODO: ボタン配置予定 */}
+       {/* // TODO: ボタン配置予定地 */}
 
         {/* ここから */}
 
-
+        
         
         {/* ここまで */}
         
