ae2599a2e291b5cf725aa58e4bfc353fac01a1da
akito <kd1373996@st.kobedenshi.ac.jp>
2026-05-07 14:30:16 +0900

    feat:記事一覧のmock作成

--- src/mocks/handlers.ts
+++
@@ -1,9 +0,0 @@
-import { http, HttpResponse } from "msw";
-
-export const handlers = [
-  // ここにモックハンドラーを追加していく
-  // 例:
-  // http.get("/api/articles", () => {
-  //   return HttpResponse.json([])
-  // }),
-];
