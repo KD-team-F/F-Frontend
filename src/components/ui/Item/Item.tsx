7fc6ccb0bc90f1d29dfafcf4707004e280803ab1
kanouyouta <zekurosu753@gmail.com>
2026-05-08 10:27:37 +0900

    Item.tsxのconst ItemにあるlikeCount, isLikedByCurrentUserに渡された値を削除した

--- src/components/ui/Item/Item.tsx
+++ src/components/ui/Item/Item.tsx
@@ -9,7 +9,7 @@ type Props = {
     isLikedByCurrentUser?: boolean;
 };
 
-export const Item = ({ title, content, date, likeCount = 0, isLikedByCurrentUser = false }: Props) => {
+export const Item = ({ title, content, date, likeCount, isLikedByCurrentUser }: Props) => {
     return (
         <div className="bg-blue-400 p-4 rounded-md mb-4 transition hover:-translate-y-1 hover:shadow-md">
             <div className="flex items-start justify-between gap-3 mb-3">
