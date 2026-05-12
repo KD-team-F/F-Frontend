f5a563ea0de8cc75cf6aa36502ddc090ca1f6d26
akito <kd1373996@st.kobedenshi.ac.jp>
2026-05-12 10:26:11 +0900

    feat:記事投稿のmockを作成

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -1,9 +1,11 @@
 import { articleListHandlers } from '@/mocks/handlers/articleList'
 import { articleDetailHandlers } from '@/mocks/handlers/articleDetail'
 import { articleCommentHandlers } from '@/mocks/handlers/articleComment'
+import { articlePostHandlers } from '@/mocks/handlers/articlePost'
 
 export const handlers = [
   ...articleListHandlers,
   ...articleDetailHandlers,
   ...articleCommentHandlers,
+  ...articlePostHandlers,
 ]
