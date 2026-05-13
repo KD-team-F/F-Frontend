3137d2922789720830fb4c1c3ff584bccdec656b
haruya8800 <kd1360860@st.kobedenshi.ac.jp>
2026-05-13 13:21:50 +0900

    EditBotton作成

---
+++ src/components/ui/EditButton/editbutton.tsx
@@ -0,0 +1,50 @@
+type EditButtonProps = {
+  onClick?: () => void
+  className?: string
+}
+
+export const EditButton = ({
+  onClick,
+  className = '',
+}: EditButtonProps) => {
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      aria-label="編集"
+      className={`flex items-center justify-center ${className}`}
+    >
+      <svg
+        width="48"
+        height="48"
+        viewBox="0 0 48 48"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        {/* ペン */}
+        <path
+          d="M10 34L14 24L30 8C31.6569 6.34315 34.3431 6.34315 36 8C37.6569 9.65685 37.6569 12.3431 36 14L20 30L10 34Z"
+          fill="#1C1C1C"
+        />
+
+        {/* ペンの白ライン */}
+        <path
+          d="M31 11L17 25"
+          stroke="white"
+          strokeWidth="2.5"
+          strokeLinecap="round"
+        />
+
+        {/* 下線 */}
+        <rect
+          x="20"
+          y="36"
+          width="14"
+          height="4"
+          rx="2"
+          fill="#1C1C1C"
+        />
+      </svg>
+    </button>
+  )
+}
