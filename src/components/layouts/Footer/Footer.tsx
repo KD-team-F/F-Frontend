18d913a5d928dc96799496c637b96cc7c3df42c0
shion1215 <kd1366608@st.kobedenshi.ac.jp>
2026-05-20 09:43:19 +0900

    バージョン変更

--- src/components/layouts/Footer/Footer.tsx
+++ src/components/layouts/Footer/Footer.tsx
@@ -1,58 +1,59 @@
+import Link from 'next/link'
 import { Mail } from 'lucide-react'
-import { FaGithub, FaInstagram, FaYoutube, FaXTwitter} from 'react-icons/fa6';
+import {
+  FaGithub,
+  FaInstagram,
+  FaYoutube,
+  FaXTwitter,
+} from 'react-icons/fa6'
 
 export function Footer() {
   return (
     <footer className="bg-[#ABE1FA] px-4 py-8 mt-auto">
       <div className="mx-auto max-w-7xl flex flex-col items-center gap-6">
 
-        {/* // TODO: ボタン配置予定地です。 */}
-
         {/* ここから */}
 
         <div className="flex items-center gap-8">
-          <a
+
+          <Link
             href="https://github.com/KD-team-F/F-Frontend"
             target="_blank"
-            rel="noopener noreferrer"
             className="hover:opacity-70 transition"
           >
             <FaGithub size={28} />
-          </a>
+          </Link>
 
-          <a
+          <Link
             href="https://www.instagram.com/kobedenshi/"
             target="_blank"
-            rel="noopener noreferrer"
             className="hover:opacity-70 transition"
           >
             <FaInstagram size={28} />
-          </a>
+          </Link>
 
-          <a
+          <Link
             href="https://www.youtube.com/user/Kobedenshi"
             target="_blank"
-            rel="noopener noreferrer"
             className="hover:opacity-70 transition"
           >
             <FaYoutube size={28} />
-          </a>
+          </Link>
 
-          <a
+          <Link
             href="https://x.com/kobedenshi?lang=ja"
             target="_blank"
-            rel="noopener noreferrer"
             className="hover:opacity-70 transition"
           >
             <FaXTwitter size={28} />
-          </a>
+          </Link>
 
-          <a
-            href="example.com"
+          <Link
+            href="mailto:example@example.com"
             className="hover:opacity-70 transition"
           >
             <Mail size={32} strokeWidth={2} />
-          </a>
+          </Link>
 
         </div>
 
