c72785276a2e13b147df166adc2f3955fb02a2bc
root <root@dsvcfoCX.>
2026-05-22 10:59:10 +0900

    getProfileの関数返り値の型定義を明確化した

--- src/features/profile/actions/getProfile.ts
+++ src/features/profile/actions/getProfile.ts
@@ -1,9 +1,14 @@
-export const getProfile = async () => {
+import type { ComponentProps } from 'react'
+import { Profile } from '@/components/ui/Profile/Profile'
+
+type ProfileData = ComponentProps<typeof Profile>
+
+export const getProfile = async (): Promise<ProfileData> => {
   const response = await fetch('/api/profile')
 
   if (!response.ok) {
     throw new Error('プロフィール情報の取得に失敗しました')
   }
 
-  return response.json()
+  return response.json() as Promise<ProfileData>
 }
